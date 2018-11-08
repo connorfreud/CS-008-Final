bf5d458c5e162cc4b0a1dcd9c4af02fb59c6c6b7
connorfreud <44878750+connorfreud@users.noreply.github.com>
2018-11-08 14:16:13 -0500

    Add files via upload

---
+++ pats_home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>New England Patriots fanclub</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Connor Freud and Sarah Clark">
+        <meta name="description" content="cs008 Final Project">
+        <style type="text/css"></style>
+    </head>
+
+    <body>
+        <p>Test Words balaeuhfd;awnes;ifdb'ANS ROEXaiur cqwae'otifowieasct R</p>
+        <figure class="Center Small">
+            <img alt="" src="images/pats_logo.jpg">
+            <figcaption>source: https://wall.alphacoders.com/big.php?i=592111</figcaption>
+        </figure>
+
+    </body>
+</html>
