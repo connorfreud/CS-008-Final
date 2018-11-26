20b7f8aab14c10aaa15630aaae0b1b383a02951c
sclark19 <44879495+sclark19@users.noreply.github.com>
2018-11-26 15:34:48 -0500

    Update pats_blog.php

--- pats_blog.php
+++ pats_blog.php
@@ -1,8 +1,23 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
+include ('pats_top.php'); 
+?>
+    <main>
+        <article>
+            <h1>Blog</h1>
+            
+            <h2>Comments Left by our Subscribers, leave yours below!</h2>
+            <p>@Marshall_Walmsley- "I really enjoy how versatile this website is. Not only can I purchase clothing, I can even customize my own! </p>
+            <p>@Maureenpatsfan1- "I like how I am always up to date on what the Patriots are doing/ what their season stats looks like</p>
+            <p>@Patrick_Kumar2323- "This website is user friendly and I never have trouble finding anything</p>
+            <p>@Brady_fan_Greenwood- "I have been a subscribers for years. This website is always changing and updating important information that all Patriots fans should know</p>
+            <p>@Koby_Adamson90- "Great work"</p>
+            <p>@gopatsgopats23- "Awesome</p>
+            <p>@12tom12- "I like how I can make my own apparel according to my own style"</p>
+            <p>@gronk87_fan- "website is always updated- nice work"</p>
+            <p>@patriotsrock_adam- "I have ordered so many shirts, I cant wait to order more"</p>
+            <p>@gillette_pats_g0- "User friendly, nice job</p>
+            <p>@Stacy_bradyGronk78- "The website is easy to navigate through and I am satisfied with the purchases I have made so far. </p>
+            <p>@CodyAustin- "Roster is accurate"</p>
+            <p>@21345Troy_Fitzgerald- "I like how I can upload my own images to be used on the site</p>
+            <p>@Clara_Haywardpats- "I come to this website each week to take the weekly poll, this website is amazing!</p>
+            
