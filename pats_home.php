c988ae2839651c3b4d8aeccccc2f0f0e74daf155
sclark19 <44879495+sclark19@users.noreply.github.com>
2018-11-26 14:01:09 -0500

    Update pats_home.php

--- pats_home.php
+++ pats_home.php
@@ -5,14 +5,30 @@ include ('pats_top.php');
         <article>
             <h2>Home</h2>
        <figure class="Homephoto">
-            <img alt="Logo" src="images/pats_logo.jpg" height='20%' width='20%'>
+           <img alt="Logo" src="images/pats_logo.jpg" height='60%' width='60%'>
             <figcaption>source: https://wall.alphacoders.com/big.php?i=592111</figcaption>
         </figure>
    
-    <p>Test Words balaeuhfd;awnes;ifdb'ANS ROEXaiur cqwae'otifowieasct R</p>
-        
-
+    <p>Welcome Patriots Fans! This is your one-stop shop for all things PATRIOTS. Here you will be able to check out the current roster of the team, shop and customize your favorite Patriots apparel, look at user-submitted images, upload comments, take a poll and so much more!</p>
    
+    <p>To get started, just click on any tab you want at the top of the screen!</p>
+    
+    <p>I’m sure most visitors are wondering, who are we? Well, we are loyal Patriots fans of course. We created this website to show just how dedicated we are. We enjoy keeping everyone up to date on the latest Patriots news and creating customizable apparel for all. Patriots pride is our number one goal. When we first started this website, we only had a few visitors come to our page. We kept expanding our site, supplying more information and started to sell apparel. When we made that huge transition is when we started to notice results. Our loyal visitors of Patriots Place spread the word of our site and helped tremendously by doing small promotional events. We truly could not have expanded our site without the help of each and every one of you. </p>
+    
+    <p>We wanted to take the time and thank our wonderful supporters. By our users submitting images, creating their own custom apparel, and by giving us feedback, we have been able to expand. The feedback is the most important attribute. Without feedback, we would not have been able to cater to each and every one of our fans.</p>
+    
+    <p>We take into consideration EVERY comment we receive, which is why we encourage you to submit feedback on our blog page! This is open to everyone who wants to share their opinions. </p>
+    
+    <p>Our main goal is to have every fan happy with our site. Our mission is to bring all the fans together and create one big “Patriots Place” experience. </p>
+    
+    <p>The two creators of this site are Sara Clark and Connor Freud. Sara has been a Patriots fan her whole life. The first time she stepped foot at Gillette Stadium was like a dream come true. She has met a few Patriots players and even has a signed football with of the players she met. Connor has also been a Patriots fan his whole life. His first Patriots game was at Gillette Stadium and they played the Giants. We came together to create this website to share our life and our passion for the Patriots. </p>
+    
+    <p>Creating this website took a lot of time and energy, and we are very happy with the outcome. We have not only gained new knowledge on creating a website, but we have created a small community of Patriots fans that came together and shared their passion for the team as well. </p>
+    
+    <p>Thank you to all of our continuing supporters. We could not have reached this goal without you. We will continue to expand our and make our supports more impressed with our website by adding new pages such as an interactive map. Our clothing company has big changes in store. We want to add more clothing options on top of our t-shirts and pants. Creating hats, scarves, mittens, gloves, socks, coats, and so much more is what we plan on developing in the near future. </p>
+   
+    <p>Thank you once again and most importantly, go Pats!</p>
+    
         </article>
     </main>
     </html>
