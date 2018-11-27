845f250599d754fde412ce9e8b30543b187df689
connorfreud <44878750+connorfreud@users.noreply.github.com>
2018-11-27 16:03:07 -0500

    Update pats_shop.php

--- pats_shop.php
+++ pats_shop.php
@@ -1,7 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include ('pats_top.php');
+?>
 <!-- Pictures of stuff to buy -->
-
+<figure>
+    <img alt="Red Shirt" src="images/Red.jpg">
+            <figcaption>source: https://d1wid1q8ctfefm.cloudfront.net/spree/images/attachments/000/005/218/original/FanaticsTBSlubLSTop.jpg?1536786193</figcaption>
+</figure>
+<figure>
+    <img alt="Navy Blue T Shirt" src="images/NavyBlue.jpg">
+            <figcaption>source: https://d1wid1q8ctfefm.cloudfront.net/spree/images/attachments/000/004/566/original/NikeCottonTeeNavy.jpg?1526653863</figcaption>
+</figure>
+<figure>
+    <img alt="Grey T Shirt" src="images/Grey.jpg">
+            <figcaption>source: https://d1wid1q8ctfefm.cloudfront.net/spree/images/attachments/000/004/722/original/NikeSlubTeeGray.jpg?1530886182</figcaption>
+</figure>
 
 <!-- Initialize variables -->
 <?php
@@ -33,12 +45,10 @@ if(isset($_POST["btnSubmit"])) {
         $msg.= '<p>Security breach reported.</p>';
         die($msg);
     }
+}
 
-?>
+// <!-- Sanitize data --> //
 
-<!-- Santize data -->
-
-<?php
 $size = htmlentities($_POST['lstSize'], ENT_QUOTES, 'UTF-8');
 $color = htmlentities($_POST['radColor'], ENT_QUOTES, 'UTF-8');
 $quantity = htmlentities($_POST['txtQuantity'], ENT_QUOTES, 'UTF-8');
@@ -62,7 +72,7 @@ $quantity = htmlentities($_POST['txtQuantity'], ENT_QUOTES, 'UTF-8');
     } elseif (!is_int($quantity)) {
         $errorMsg[] = 'Enter a number';
         $quantityERROR = true;
-        
+    }    
     if ($email == ""){
         $errorMsg[] = 'Please enter your email address';
         $emailERROR = true;
@@ -76,9 +86,9 @@ $quantity = htmlentities($_POST['txtQuantity'], ENT_QUOTES, 'UTF-8');
 <!-- if form is valid, save the data -->    
 <?php
 if (!$errorMsg){
-        if ($debug)
+        if ($debug) {
                 print '<p>Form is valid.</p>';
-    
+        } 
     
     // array used to hold form values that will be saved to csv
     $dataRecord = array();
@@ -104,12 +114,13 @@ if (!$errorMsg){
     
     // close file
     fclose($file);
-    ?>
- <!-- create order confirmation and mail to user -->   
-<?php
+
+ // create order confirmation and mail to user -->   
+
+
 $message = '<h2> Your order confirmation </h2>';
     
-    foreach ($_POST as $htmlName => $value) {
+   foreach ($_POST as $htmlName => $value) {
     
         $message .= '<p>';
         $camelCase = preg_split('/(?=[A-Z])/', substr($htmlName, 3));
@@ -126,100 +137,102 @@ $message = '<h2> Your order confirmation </h2>';
     $bcc = '';
     
     $from = '<PatriotsPlaceClub@Patriots.com>';
-    
-    
+//    
+//    
     $subject = 'Order Confirmation from Patriots Place';
-    
+//    
     $mailed = sendMail($to, $cc, $bcc, $from, $subject, $message);
-    ?>
+}
+//   ?>
 
 <!-- Form to choose color(list), size(radio), quantity(Num input) -->
+
 <main>
     <article>
-<form action ='<?php print $phpSelf; ?>'
-       id ='frmOrder'
-       method="post">
-     
-     <fieldset class ='order'>
-         <legend>Order Information</legend>
-         <fieldset class="listbox <?php if ($sizeERROR) print 'mistake';?>">
-             <p>
-             <legend> Select a Size </legend>
-             <select id="lstSize"
-                     name="lstSize"
-                     tabindex="100">
-                 <option <?php if ($size == 'Small') print 'selected'; ?>
-                     value ='Small'>Small</option>
-                 
-                 <option <?php if ($size == 'Medium') print 'selected'; ?>
-                     value ='Medium'>Medium</option>
-                 
-                 <option <?php if ($size == 'Large') print 'selected'; ?>
-                     value ='Large'>Large</option>
-                 
-                 <option <?php if ($size == 'X Large') print 'selected'; ?>
-                     value ='x Large'>X Large</option>
-             </select>
-         </p>
-         </fieldset>
-         
-         <fieldset class="radio <?php if ($colorERROR) print 'mistake'; ?>">
-             <legend> Select a color</legend>
-             <p>
-                 <label class="radio-field"><input type="radio" id="radColorRed" name="radColor" value="Red" tabindex="200" 
-<?php if ($color == "Red") echo ' checked="checked" '; ?>>
-            Red</label>
-             </p>
-             
-             <p>
-                 <label class="radio-field"><input type="radio" id="radColorNavyBlue" name="radColor" value="Navy Blue" tabindex="201" 
-<?php if ($color == "Navy Blue") echo ' checked="checked" '; ?>>
-            Navy Blue</label>
-             </p>
-             
-             <p>
-                 <label class="radio-field"><input type="radio" id="radColorGrey" name="radColor" value="Grey" tabindex="202" 
-<?php if ($color == "Grey") echo ' checked="checked" '; ?>>
-            Grey</label>
-             </p>
-             
-             
-         </fieldset>
-         
-         <p>
-             <label class ='required' for='txtQuantity'>Quantity</label>
-             <input autofocus
-                    <?php if ($quantityERROR) print 'class="mistake"'; ?>
-                    id="txtQuantity"
-                    maxlength="3"
-                    name="txtQuantity"
-                    onfocus="this.select()"
-                    placeholder="Enter the quantity"
-                    tabindex="300"
-                    type="text"
-                    value="<?php print $quantity; ?>"
-                    >
-         </p>
-         
-         <p>
-             <label class='required' for="txtEmail">Email</label>
-             <input
-                 <?php if ($emailERROR) print 'class="mistake"'; ?>
-                 id="txtEmail"
-                 maxlength="50"
-                 name="txtEmail"
-                 onfocus="this.select()"
-                 placeholder="Enter your Email address"
-                 tabindex="400"
-                 type="text"
-                 value="<?php print $email; ?>"
-               >
-         </p>
-         <?php
-}
-    }
-}
-?>
-         </article>
+                    <form action="<?php print $phpSelf; ?>"
+                           id="frmOrder"
+                           method="post">
+
+                         <fieldset class='order'>
+                             <legend>Order Information</legend>
+                             <fieldset class="listbox <?php if ($sizeERROR) print 'mistake';?>">
+                                 <p>
+                                 <legend> Select a Size </legend>
+                                 <select id="lstSize"
+                                         name="lstSize"
+                                         tabindex="100">
+                                     <option <?php if ($size == 'Small') print 'selected'; ?>
+                                         value ='Small'>Small</option>
+
+                                     <option <?php if ($size == 'Medium') print 'selected'; ?>
+                                         value ='Medium'>Medium</option>
+
+                                     <option <?php if ($size == 'Large') print 'selected'; ?>
+                                         value ='Large'>Large</option>
+
+                                     <option <?php if ($size == 'X Large') print 'selected'; ?>
+                                         value ='x Large'>X Large</option>
+                                 </select>
+                             </p>
+                             </fieldset>
+                         </fieldset>
+
+                             <fieldset class="radio <?php if ($colorERROR) print 'mistake'; ?>">
+                                 <legend> Select a color</legend>
+                                 <p>
+                                     <label class="radio-field"><input type="radio" id="radColorRed" name="radColor" value="Red" tabindex="200" 
+                    <?php if ($color == "Red") echo ' checked="checked" '; ?>>
+                                Red</label>
+                                 </p>
+
+                                 <p>
+                                     <label class="radio-field"><input type="radio" id="radColorNavyBlue" name="radColor" value="Navy Blue" tabindex="201" 
+                    <?php if ($color == "Navy Blue") echo ' checked="checked" '; ?>>
+                                Navy Blue</label>
+                                 </p>
+
+                                 <p>
+                                     <label class="radio-field"><input type="radio" id="radColorGrey" name="radColor" value="Grey" tabindex="202" 
+                    <?php if ($color == "Grey") echo ' checked="checked" '; ?>>
+                                Grey</label>
+                                 </p>
+
+
+                             </fieldset>
+
+                             <p>
+                                 <label class ='required' for='txtQuantity'>Quantity</label>
+                                 <input autofocus
+                                        <?php if ($quantityERROR) print 'class="mistake"'; ?>
+                                        id="txtQuantity"
+                                        maxlength="3"
+                                        name="txtQuantity"
+                                        onfocus="this.select()"
+                                        placeholder="Enter the quantity"
+                                        tabindex="300"
+                                        type="text"
+                                        value="<?php print $quantity; ?>"
+                                        >
+                             </p>
+
+                             <p>
+                                 <label class='required' for="txtEmail">Email</label>
+                                 <input
+                                     <?php if ($emailERROR) print 'class="mistake"'; ?>
+                                     id="txtEmail"
+                                     maxlength="50"
+                                     name="txtEmail"
+                                     onfocus="this.select()"
+                                     placeholder="Enter your Email address"
+                                     tabindex="400"
+                                     type="text"
+                                     value="<?php print $email; ?>"
+                                   >
+                             </p>
+                             <fieldset class="buttons">
+                             <input class="button" id="btnSubmit" name="btnSubmit" tabindex="500" type="submit" value="Submit Order" >
+                         </fieldset>
+                    </form>
+         </article>   
 </main>
 </html>
