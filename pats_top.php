ef2491310c21b55b8cd169d01f12f7c401575b47
sclark19 <44879495+sclark19@users.noreply.github.com>
2018-12-03 14:14:01 -0500

    Update pats_top.php

--- pats_top.php
+++ pats_top.php
@@ -1,16 +1,23 @@
+<?php
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
 
+$path_parts = pathinfo($phpSelf);
+
+ ?>	
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Patriots Place</title>
         <meta charset="utf-8">
-        <meta name="author" content="Connor Freud and Sarah Clark">
+        <meta name="author" content="Connor Freud and Sara Clark">
         <meta name="description" content="cs008 Final Project">
         
         <link rel="stylesheet" href="finalcss.css" type="text/css" media="screen">
     </head>
 
 <?php
+print '<body id="' . $path_parts['filename'] . '">' . PHP_EOL;
+
 include ('pats_header.php');
 print PHP_EOL;
 
