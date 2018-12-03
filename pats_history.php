ed6a678865c44c024f5a6d6b9f36994aa4c4738a
sclark19 <44879495+sclark19@users.noreply.github.com>
2018-12-03 14:12:40 -0500

    Update and rename pats_poll.php to pats_history.php

---
+++ pats_history.php
@@ -0,0 +1,26 @@
+<?php 
+include ('pats_top.php'); 
+?>
+    <main>
+        <article>
+            <h2>History</h2>
+            <header>Welcome Pats Fans!</header>
+       
+   
+    <p>The New England Patriots football team was first established in 1960 when they joined the NFL franchise. During this time, they were known as the Boston Patriots, located in Boston Massachusetts. The Boston Patriots initially played their home games at Boston University's Nickerson Field. The switch of stadiums from Nickerson Field to Foxborough Stadium (now known as Gillette Stadium which opened in 2002) took place in 1971 when their name changed to the New England Patriots. The New England Patriots was not the initial name they were going to use. After being known as the Boston Patriots, their name was set to change to the Bay State Patriots, but this name was shot down by a lot of fans. The name change to The New England Patriots occurred in 1971. During the 1960 season, the Boston Patriots finished their season with a total of five wins and nine losses. The head coach of this team was Lou Saban.  </p>
+    
+    <p>It is no secret that the New England Patriots are one of the best teams in the NFL. The Patriots have had 59 seasons so far to date with the first one beginning in 1960. The team has won five Super Bowl titles with 10 appearances. The titles were earned in years: 2002, 2004, 2005, 2015, and 2017. On top of that, the team has won ten AFC games. The first ever Super Bowl championship game took place in 1967 before the Patriots were even apart of the National Football League. The Patriots currently hold the record for most consecutive appearances in the Super Bowl Championship game with a total running on 9 seasons.</p>
+    
+    <p>In 2000, the New England Patriots received a new coach Bill Belichick. Belichick lead his team to multiple titles, and this was one of best moves the franchise could have made. Bill is not only a great coach, but he is smart with his plays as well. </p>
+    
+    <p>The team was first owned by Billy Sullivan in 1960-1987. After two more changes, Robert Kraft owned the Patriots. This took place in 1994, and he is still the owner to this day. In 1993, the team announces their mascot as Pat Patriot. This is still the mascot to this day, as well!</p>
+    
+    <p>Tom Brady is a known figure when thinking about the New England Patriots. Brady is the starting quarterback and very well is one of the best players the Patriots have ever had. Brady was drafted to the NFL in 2000 to the Patriots during the sixth round. Brady was not only a football player throughout grade school, however. Brady excelled at baseball and was even offered to play pro. He turned that opportunity down to go to college in Michigan. During Brady’s first NFL appearance, he was not the star. He was the backup quarterback. After the starting quarterback was injured, Brady got his chance, and he shined. Brady’s record is incredible. He has won five Super Bowl championships with the New England Patriots and appeared in eight games. </p>
+    
+    <p>The New England Patriots have a lot of history behind their team, and it is evident they are coached extremely well. The 2018-2019 season will be one to remember!</p>
+    
+        </article>
+    </main>
+</html>
+
+   
