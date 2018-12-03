66fb8a7d25e0c61f9ed7ebc4488e3402073fe071
sclark19 <44879495+sclark19@users.noreply.github.com>
2018-12-03 14:15:55 -0500

    Update pats_roster.php

--- pats_roster.php
+++ pats_roster.php
@@ -1,21 +1,21 @@
 <?php
 include ('pats_top.php');
-
 $roster = '';
 if (isset($_GET['roster'])) {
     $roster = htmlentities($_GET[roster], ENT_QUOTES, "UTF-8");
 }
+
+?>
+<main>
+        <article>
+            <h2>Roster</h2>
+<?php
 // Open roster csv
 $debug = false;
 if (isset($_GET["debug"])) {
     $debug = true;
 }
-
-
-
-
-
-echo "<html><body><table>\n\n";
+echo "<table>\n\n";
 $file = fopen("data/roster.csv", "r");
 while (($line = fgetcsv($file)) !== false) {
         echo "<tr>";
@@ -25,7 +25,6 @@ while (($line = fgetcsv($file)) !== false) {
         echo "</tr>\n";
 }
 fclose($f);
-echo "\n</table></body></html>";
-
+echo "\n</table></article></main></body></html>";
 fclose($file);
 ?>
