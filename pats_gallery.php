a79afcc49b645c031a074b88cd992e0fff032dec
connorfreud <44878750+connorfreud@users.noreply.github.com>
2018-12-05 16:06:45 -0500

    Update pats_gallery.php

--- pats_gallery.php
+++ pats_gallery.php
@@ -1,8 +1,31 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
+include ('pats_top.php'); 
+?>
+<main>
+    <figure id='slideshow'>
+        <figure>
+            <img src="images/Gallery1.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+        <figure>
+            <img src="images/Gallery2.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+        <figure>
+            <img src="images/Gallery3.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+        <figure>
+            <img src="images/Gallery4.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+        <figure>
+            <img src="images/Gallery5.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+        <figure>
+            <img src="images/Gallery6.jpg">
+            <figcaption>Images from APimages.com</figcaption>
+        </figure>
+    </figure>
+</main>
